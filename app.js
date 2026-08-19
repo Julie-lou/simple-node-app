a566f7a79316c6410f2b942b7bf6c818d91d1acb
Julie Lou <2023169lou@aupp.edu.kh>
2026-08-19 16:42:18 +0700

    Update app for v2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello this is VERSION 2 branch!");
+  res.send("HELLO FROM V2");
 });
 
 app.get("/health", (req, res) => {
